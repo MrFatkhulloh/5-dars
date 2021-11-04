916c7ecbb4f613029a840c7968b72b66080d123e
MrFatkhulloh <eternallfame@gmail.com>
2021-11-04 20:47:22 +0500

    add some changes

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
 // console.log("any changes")
+var userMoney = prompt("How much money do you have? (in soums please!)");
 
 var planeTicket = 500;
 var hotelCost = 250;
@@ -7,9 +8,9 @@ var museumCost = 120;
 var euro = prompt("how much does 1 euro costs in soum?");
 var dollar = prompt("how much does 1$ costs in soum?");
 
-var userMoneySoum = (planeTicket+hotelCost)*dollar+museumCost*euro
+var totalCostSoum = Math.ceil((planeTicket+hotelCost)*dollar+museumCost*euro)
+var userMoneySoum = Math.floor(userMoney)
 
-var userMoney = prompt("How much money do you have? (in soums please!)");
 if (userMoney >= (planeTicket+hotelCost)*dollar+museumCost*euro) {
     console.log(`1$ - ${dollar} soums;\n1euro - ${euro} soums; \ntotal cost - ${(planeTicket+hotelCost)*dollar+museumCost*euro} soums; \nYou have - ${userMoney} soums and it's enough!;\nOq yo’l, Alisher!`);
     alert(`1$ - ${dollar} soums;\n1euro - ${euro} soums; \ntotal cost - ${(planeTicket+hotelCost)*dollar+museumCost*euro} soums; \nYou have - ${userMoney} soums and it's enough!;\nOq yo’l, Alisher!`);
